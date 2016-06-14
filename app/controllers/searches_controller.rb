08df7f2225c60af9d23e457e73ba6a6d396b3025
Matt Pindell <pindell.m@gmail.com>
2016-06-14 10:16:23 -0600

    Create searches controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,8 @@
+class SearchesController < ApplicationController
+
+  def index
+    require "pry"
+    binding.pry
+  end
+
+end
--- spec/features/user_can_search_stores_by_zip_spec.rb
+++ spec/features/user_can_search_stores_by_zip_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "user can search for closest stores with their zip code" do
-  VCR.use_cassette("synopsis") do
+  it "can enter zip code into search form" do
+  # VCR.use_cassette("search/zip") do
+  # create user
+
+  visit root_path
+
+  within('.search-form') do
+    fill_in :zip, with: "80202"
+    click_button "search"
+  end
+
+  expect(current_path).to eq('/search')
+
+  # And I should see stores within 25 miles of 80202
+  # And I should see a message that says "17 Total Stores"
+  # And I should see exactly 15 results
+  # And I should see the long name, city, distance, phone number and store type for each of the 15 results
 
    end
 end
